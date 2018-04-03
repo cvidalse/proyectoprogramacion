3ab7c376e6b4ebdb2ae3fa7f8832f961003b4aad
cvidalse <37351971+cvidalse@users.noreply.github.com>
2018-04-03 18:55:01 -0300

    codigo en arduinos

---
+++ prueba1.ino
@@ -0,0 +1,19 @@
+int val=0;
+int pinInt = 4;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(pinInt,INPUT);
+ Serial.begin(9600);
+ while(Serial.available()){
+  Serial.read();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = digitalRead(pinInt);
+Serial.print(val);
+delay(1000);
+}
+
+
